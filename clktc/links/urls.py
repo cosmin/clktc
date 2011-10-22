e12e49640e85f95d65fbb01db74b33a71aeeb2b4
Cosmin Stejerean <cosmin@offbytwo.com>
2011-10-22 18:59:13 +0800

    added view for getting all links

---
+++ clktc/links/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls.defaults import patterns, include, url
+from clktc.links import views
+
+urlpatterns = patterns('',
+    url(r'^links/', views.get_all_links),
+)
--- clktc/links/views.py
+++ clktc/links/views.py
@@ -1,1 +1,9 @@
 # Create your views here.
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
+from clktc.links.models import Link
+
+def get_all_links(request):
+    return render_to_response("links/all.html", RequestContext(request, dict(
+        links = Link.objects.all(),
+    )))
