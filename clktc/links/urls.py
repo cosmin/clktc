f18a385ee0b7d1f591c32cf32babe81ed8fec387
Cosmin Stejerean <cosmin@offbytwo.com>
2011-10-29 19:44:30 +0800

    added view and url for adding a new site

--- clktc/links/urls.py
+++ clktc/links/urls.py
@@ -3,4 +3,5 @@ from clktc.links import views
 
 urlpatterns = patterns('',
     url(r'^links/', views.get_all_links),
+    url(r'^add/', views.add_link),
 )
--- clktc/links/views.py
+++ clktc/links/views.py
@@ -1,5 +1,6 @@
 # Create your views here.
-from django.shortcuts import render_to_response
+from django.contrib.sites.models import get_current_site
+from django.shortcuts import render_to_response, redirect
 from django.template.context import RequestContext
 from clktc.links.models import Link
 
@@ -7,3 +8,12 @@ def get_all_links(request):
     return render_to_response("links/all.html", RequestContext(request, dict(
         links = Link.objects.all(),
     )))
+
+
+def add_link(request):
+    short_url = request.POST['short_url']
+    destination_url = request.POST['destination_url']
+    link = Link(short_url=short_url, destination_url=destination_url)
+    link.site = get_current_site(request)
+    link.save()
+    return redirect(get_all_links)
