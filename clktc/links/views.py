38213b6e356a89b096495c7aad2b5c864f679bf9
Cosmin Stejerean <cosmin@offbytwo.com>
2011-10-29 19:54:14 +0800

    added view and template for adding a new link

--- clktc/links/views.py
+++ clktc/links/views.py
@@ -11,9 +11,12 @@ def get_all_links(request):
 
 
 def add_link(request):
-    short_url = request.POST['short_url']
-    destination_url = request.POST['destination_url']
-    link = Link(short_url=short_url, destination_url=destination_url)
-    link.site = get_current_site(request)
-    link.save()
-    return redirect(get_all_links)
+    if request.method == "GET":
+        return render_to_response("links/add.html", RequestContext(request))
+    else:
+        short_url = request.POST['short_url']
+        destination_url = request.POST['destination_url']
+        link = Link(short_url=short_url, destination_url=destination_url)
+        link.site = get_current_site(request)
+        link.save()
+        return redirect(get_all_links)
