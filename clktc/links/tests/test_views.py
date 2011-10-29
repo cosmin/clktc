7289362d213b2c8b0f711973eb7deb219af0b087
Cosmin Stejerean <cosmin@offbytwo.com>
2011-10-29 19:31:55 +0800

    asserting links are passed in context instead of asserting template rendering

--- clktc/links/tests/test_views.py
+++ clktc/links/tests/test_views.py
@@ -1,15 +1,17 @@
 from django.contrib.sites.models import Site
+from django.http import HttpRequest
 from django.test.client import Client
 from django.test import TestCase
 from clktc.links.models import Link
+from clktc.links.views import get_all_links
 
 class ViewTests(TestCase):
     def setUp(self):
         self.client = Client()
 
     def test_get_all_links(self):
-        Link(destination_url="http://example.com", short_url="example", site=Site.objects.get(pk=1)).save()
+        link = Link(destination_url="http://example.com", short_url="example", site=Site.objects.get(pk=1))
+        link.save()
         response = self.client.get('/links/')
-        self.assertContains(response, '<a href="http://example.com">example</a>')
-        
+        self.assertIn(link, response.context['links'])
 
