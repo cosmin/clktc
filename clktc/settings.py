e992a5a8c948a51f7e468824cd344be75104eed2
Cosmin Stejerean <cosmin@offbytwo.com>
2011-10-30 14:41:57 +0800

    added twitter bootstrap and edited base template to use it

--- clktc/settings.py
+++ clktc/settings.py
@@ -52,6 +52,7 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, "static"),
 )
 
 # List of finder classes that know how to find static files in
