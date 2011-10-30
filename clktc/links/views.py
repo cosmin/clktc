8559a6d578eaa89813e9fbf2bee6652b3096a6a8
Cosmin Stejerean <cosmin@offbytwo.com>
2011-10-30 11:22:17 +0800

    redirecting to all links view on edit

--- clktc/links/views.py
+++ clktc/links/views.py
@@ -24,8 +24,10 @@ def add_link(request):
 
 def edit_link(request, link_id):
     link = get_object_or_404(Link, pk=link_id)
-    if request.method == "POST":
+    if request.method == "GET":
+        return render_to_response("links/edit.html", RequestContext(request, dict(link=link)))
+    elif request.method == "POST":
         link.destination_url = request.POST['destination_url']
         link.short_url = request.POST['short_url']
         link.save()
-    return render_to_response("links/edit.html", RequestContext(request, dict(link=link)))
+        return redirect(get_all_links)
