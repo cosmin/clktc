62bc928745888294456f025c256ab4adbb3cbe8d
Cosmin Stejerean <cosmin@offbytwo.com>
2011-10-30 11:19:39 +0800

    refactored view tests to have more meaningful names

--- clktc/links/tests/test_views.py
+++ clktc/links/tests/test_views.py
@@ -15,26 +15,27 @@ class ViewTests(TestCase):
     def setUp(self):
         self.client = Client()
 
-    def test_get_all_links(self):
-        link = Link(destination_url="http://example.com", short_url="example", site=Site.objects.get(pk=1))
-        link.save()
+    def test_get_all_links_returns_all_links_to_all_template(self):
+        link = given_a_link()
         response = self.client.get('/links/')
         self.assertIn(link, response.context['links'])
         self.assertEqual(response.templates[0].name, "links/all.html")
 
-    def test_visit_add_link_page(self):
+    def test_add_link_returns_add_link_template(self):
         response = self.client.get('/add/')
         self.assertEqual(response.templates[0].name, "links/add.html")
 
-    def test_add_link(self):
-        response = self.client.post('/add/', dict(destination_url=EXAMPLE_URL, short_url="example"))
+    def test_adding_a_link_creates_link(self):
+        self.client.post('/add/', {"destination_url" : EXAMPLE_URL, "short_url" : "example"})
         link = Link.objects.get(short_url="example")
         self.assertEqual(link.destination_url, EXAMPLE_URL)
+
+    def test_adding_a_link_redirects_to_all_links_page(self):
+        response = self.client.post('/add/', {"destination_url" : EXAMPLE_URL, "short_url" : "example"})
         self.assertRedirects(response, "/links/")
 
     def test_edit_link_returns_correct_link_to_edit_template(self):
-        link = Link(destination_url="http://example.com", short_url="example", site=Site.objects.get(pk=1))
-        link.save()
+        link = given_a_link()
         response = self.client.get("/edit/%s" % link.pk)
         self.assertEqual(response.templates[0].name, "links/edit.html")
         self.assertEqual(response.context['link'], link)
