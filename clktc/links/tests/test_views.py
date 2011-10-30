f961013189dc1c52c4ffb4ec5a848481e681cfdb
Cosmin Stejerean <cosmin@offbytwo.com>
2011-10-30 11:26:40 +0800

    changed url structure to add /l/ to all links

--- clktc/links/tests/test_views.py
+++ clktc/links/tests/test_views.py
@@ -17,38 +17,38 @@ class ViewTests(TestCase):
 
     def test_get_all_links_returns_all_links_to_all_template(self):
         link = given_a_link()
-        response = self.client.get('/links/')
+        response = self.client.get('/l/links/')
         self.assertIn(link, response.context['links'])
         self.assertEqual(response.templates[0].name, "links/all.html")
 
     def test_add_link_returns_add_link_template(self):
-        response = self.client.get('/add/')
+        response = self.client.get('/l/add/')
         self.assertEqual(response.templates[0].name, "links/add.html")
 
     def test_adding_a_link_creates_link(self):
-        self.client.post('/add/', {"destination_url" : EXAMPLE_URL, "short_url" : "example"})
+        self.client.post('/l/add/', {"destination_url" : EXAMPLE_URL, "short_url" : "example"})
         link = Link.objects.get(short_url="example")
         self.assertEqual(link.destination_url, EXAMPLE_URL)
 
     def test_adding_a_link_redirects_to_all_links_page(self):
-        response = self.client.post('/add/', {"destination_url" : EXAMPLE_URL, "short_url" : "example"})
-        self.assertRedirects(response, "/links/")
+        response = self.client.post('/l/add/', {"destination_url" : EXAMPLE_URL, "short_url" : "example"})
+        self.assertRedirects(response, "/l/links/")
 
     def test_edit_link_returns_correct_link_to_edit_template(self):
         link = given_a_link()
-        response = self.client.get("/edit/%s" % link.pk)
+        response = self.client.get("/l/edit/%s" % link.pk)
         self.assertEqual(response.templates[0].name, "links/edit.html")
         self.assertEqual(response.context['link'], link)
 
 
     def test_save_on_edit_link_updates_link_with_new_details(self):
         link = given_a_link()
-        self.client.post("/edit/%s" % link.pk, {"destination_url" : "http://example.org", "short_url" : "example2"})
+        self.client.post("/l/edit/%s" % link.pk, {"destination_url" : "http://example.org", "short_url" : "example2"})
         link = Link.objects.get(pk=link.pk)
         self.assertEqual(link.destination_url, "http://example.org")
         self.assertEqual(link.short_url, "example2")
 
     def test_save_on_edit_link_redirects_to_all_links(self):
         link = given_a_link()
-        response = self.client.post("/edit/%s" % link.pk, {"destination_url" : "http://example.org", "short_url" : "example2"})
-        self.assertRedirects(response, "/links/")
+        response = self.client.post("/l/edit/%s" % link.pk, {"destination_url" : "http://example.org", "short_url" : "example2"})
+        self.assertRedirects(response, "/l/links/")
--- clktc/links/urls.py
+++ clktc/links/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import patterns, include, url
 from clktc.links import views
 
 urlpatterns = patterns('',
-    url(r'^links/', views.get_all_links),
-    url(r'^add/', views.add_link),
-    url(r'^edit/(?P<link_id>[0-9]+)', views.edit_link),
+    url(r'^l/links/', views.get_all_links),
+    url(r'^l/add/', views.add_link),
+    url(r'^l/edit/(?P<link_id>[0-9]+)', views.edit_link),
 )
