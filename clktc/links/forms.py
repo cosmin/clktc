77b023da94ee7e510625fc288bc9148ba191a5be
Cosmin Stejerean <cosmin@offbytwo.com>
2011-10-30 16:35:23 +0800

    using bootstrap form for adding a new link

--- clktc/links/forms.py
+++ clktc/links/forms.py
@@ -1,7 +1,13 @@
+from django.forms import forms
 from django.forms.models import ModelForm
+from clktc.bootstrap.forms import BootstrapModelForm
 from clktc.links.models import Link
 
-class LinkForm(ModelForm):
+class LinkForm(BootstrapModelForm):
     class Meta:
         model = Link
         fields = ('short_url', 'destination_url')
+        widgets = {
+            'destination_url': forms.TextInput,
+            'short_url': forms.TextInput,
+        }
